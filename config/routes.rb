16a313900d9965fb2655ca422310d671ba921736
NjeriChelimo <martha.c.chumo@gmail.com>
2013-07-01 12:13:51 +0300

    put together the new simpler design

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 NdsStagingB::Application.routes.draw do
+  resources :homes
+  root :to => 'homes#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,6 @@ NdsStagingB::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 

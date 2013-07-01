d78bc15dbfec645f8cb17eac015544924eeb295c
NjeriChelimo <martha.c.chumo@gmail.com>
2013-07-01 11:05:43 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NdsStagingB::Application.config.secret_token = '6a477cb800f444352de8eca6879fcdc5805791f3113a2d06723235a6d98df4902e51edfa3e5dd5547cfab008bd1d5f3e98a9d225bc21dc11bfa73d8015f3a079'
